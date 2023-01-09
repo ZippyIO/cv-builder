37c0faf05553f9b390de3673538f3c8e12a9d0a8
Nick <ZippyIO@users.noreply.github.com>
2023-01-10 10:46:59 +1100

    GeneralForm removed handleInput and states

--- src/components/form/GeneralForm.jsx
+++ src/components/form/GeneralForm.jsx
@@ -3,28 +3,18 @@ import React, { Component } from 'react';
 class GeneralForm extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            name: '',
-            email: '',
-            phone: '',
-        };
     }
-
-    handleInput = (event) => {
-        this.setState({ [event.target.id]: event.target.value });
-    };
 
     render() {
         return (
             <div>
                 <form id="generalForm" className="generalForm" onSubmit={this.props.onSubmitForm}>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" onChange={this.handleInput} />
+                    <input type="text" id="name" />
                     <label htmlFor="email">Email:</label>
-                    <input type="text" id="email" onChange={this.handleInput} />
+                    <input type="text" id="email" />
                     <label htmlFor="phone">Phone:</label>
-                    <input type="text" id="phone" onChange={this.handleInput} />
+                    <input type="text" id="phone" />
                     <button type="submit">Add General Info</button>
                 </form>
                 <div></div>
