a785788cb22f1923d9fa79382f6364a17efb87aa
Nick <ZippyIO@users.noreply.github.com>
2023-01-11 11:24:02 +1100

    GeneralForm added hiding form if not in edit mode

--- src/components/form/GeneralForm.jsx
+++ src/components/form/GeneralForm.jsx
@@ -6,17 +6,20 @@ class GeneralForm extends Component {
     }
 
     render() {
-        return (
-            <form id="generalForm" onSubmit={this.props.onSubmitForm}>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" />
-                <label htmlFor="email">Email:</label>
-                <input type="text" id="email" />
-                <label htmlFor="phone">Phone:</label>
-                <input type="text" id="phone" />
-                <button type="submit">Add General Info</button>
-            </form>
-        );
+        const { ...data } = this.props.cvData;
+        if (this.props.edit) {
+            return (
+                <form id="generalForm" onSubmit={this.props.onSubmitForm}>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" defaultValue={data.name} />
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" id="email" defaultValue={data.email} />
+                    <label htmlFor="phone">Phone:</label>
+                    <input type="text" id="phone" defaultValue={data.phone} />
+                    <button type="submit">Add General Info</button>
+                </form>
+            );
+        }
     }
 }
 
